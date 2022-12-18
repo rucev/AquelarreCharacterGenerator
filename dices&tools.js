242c9e30347b6dab1ff18af0063ad3df2c2d4daf
rucev <ruedaflors@gmail.com>
2022-12-18 13:52:43 +0100

    Add rollRange tool

---
+++ dices&tools.js
@@ -0,0 +1,24 @@
+const rollD4=()=> {
+    return Math.floor(Math.random() * (4) + 1);
+}
+
+const rollD6=()=> {
+    return Math.floor(Math.random() * (6) + 1);
+}
+
+const rollD8=()=> {
+    return Math.floor(Math.random() * (8) + 1);
+}
+
+const rollD10=()=> {
+    return Math.floor(Math.random() * (10) + 1);
+}
+
+const rollD100=()=> {
+    return Math.floor(Math.random() * (100) + 1);
+}
+
+const rollRange=(start, end)=> { return new Array(end+1 - start).fill().map((d, i) => i + start) };
+
+
+export {rollD4, rollD6, rollD8, rollD10, rollD100, rollRange};
